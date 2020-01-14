77c6fc92b3e1e187a02abdbe593c9f5f4f529e88
Asha Kang <asha172@mac.local>
2020-01-14 15:31:18 +0900

    dfdfd

---
+++ fhir_reg/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ResourceDetails from './components/ResourceDetails';
+import Say from './components/Say';
+
+
+
+function App() {
+  return  <ResourceDetails/>
+}
+
+export default App;
---
+++ fhir_reg/src/components/ResourceDetails.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+// import fhir  from 'FHIR'
+// import {} from 'fhir'
+
+import fhir from 'node_modules/fhir/dist/bundle.js'
+
+const ResourceDetails = () => {
+// fh.
+//   var parser = new fh.ParseConformance(false);
+// JSON.parse(fs.)
+  return (
+    <div>
+      FHIR!!!
+    </div>
+  );
+};
+
+export default ResourceDetails;
